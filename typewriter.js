1a2eb85eea105d5f134645f83dad910fa976254c
Bernard Yang <ybernard@gmail.com>
2021-06-01 21:05:13 +0000

    removed 50 ms from \n

--- typewriter.js
+++ typewriter.js
@@ -4,10 +4,10 @@ for (const char of sentence) {
   setTimeout(() => {
     process.stdout.write(char);
   }, iterations += 50);
-  
+
 }
 setTimeout(() => {
   process.stdout.write('\n');
-}, iterations += 50);
+}, iterations);
 
 
