cc27461558009a62828989c571c35eac1b0ec3c8
Bernard Yang <ybernard@gmail.com>
2021-06-01 20:46:11 +0000

    typewriter exercise with 50ms delay each character

---
+++ typewriter.js
@@ -0,0 +1,13 @@
+const sentence = "hello there from lighthouse labs";
+let iterations = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, iterations += 50);
+  
+}
+setTimeout(() => {
+  process.stdout.write('\n');
+}, iterations += 50);
+
+
